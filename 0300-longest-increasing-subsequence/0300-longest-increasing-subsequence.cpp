8fe9047ee99903fe36e7be3c2705a4f28e352cb3
Wu Ruiyi <1509989070@qq.com>
2026-04-10 20:52:19 +0800

    Time: 69 ms (50.5%), Space: 14.5 MB (43.15%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums)
+    {
+        vector<int> dp(nums.size(),1);
+        int maxlen = 1;
+        for(int i = 1;i < nums.size();i++)
+        {
+            for(int j = 0;j < i;j++)
+            {
+                if(nums[j] < nums[i])
+                   dp[i] =max(dp[j]+1,dp[i]);
+            }
+            if(dp[i] > maxlen) maxlen = dp[i];
+        }
+        return maxlen;
+    }
+};
