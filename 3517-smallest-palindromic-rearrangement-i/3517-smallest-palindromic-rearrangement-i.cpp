034ac23a68b7c8d862ebce715c26ecbe983a9582
Wu Ruiyi <1509989070@qq.com>
2026-07-28 13:20:17 +0800

    Time: 23 ms (82.86%), Space: 60.9 MB (80%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -3,8 +3,28 @@ public:
     string smallestPalindrome(string s)
     {
         int size = s.size();
-        sort(s.begin(),s.begin()+size/2);
-        sort(s.begin()+size/2+size%2,s.end(),greater<char>());
-        return s;
+        vector<int> v(26,0);
+        for(int i = 0;i < size / 2;i++)
+        {
+           v[s[i]-'a']++;
+        }
+        int index = 0;
+        string ans(size,' ');
+        for(int i = 0;i < 26;i++)
+        {
+             for(int j = 0;j < v[i];j++)
+            {
+                ans[index++] = i + 'a'; 
+            }
+        }
+        if(size % 2) ans[index++]=s[size/2];
+        for(int i = 25;i >= 0;i--)
+        {
+            for(int j = 0;j < v[i];j++)
+            {
+                ans[index++] = i + 'a'; 
+            }
+        }
+        return ans;
     }
 };
